7c0ff924be02eff22d39633247686ceaff0e9b46
pawelsawicz <pawel@sawicz.eu>
2014-08-30 13:33:01 +0200

    speakers class has been created

---
+++ DevDay.Mobile.WP8.Data/Speakers/SebRose.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDay.Mobile.WP8.Data.Speakers
+{
+    public class SebRose : Speaker
+    {
+
+    }
+}
---
+++ DevDay.Mobile.WP8.Data/Speakers/SimonBrown.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDay.Mobile.WP8.Data.Speakers
+{
+    public class SimonBrown : Speaker
+    {
+    }
+}
