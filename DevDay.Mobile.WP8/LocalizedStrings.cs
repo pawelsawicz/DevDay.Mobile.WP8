34fc372ecb1fc7252f00e5c8dd4acc63dc59f242
pawelsawicz <pawel@sawicz.eu>
2014-08-30 12:57:23 +0200

    project init

---
+++ DevDay.Mobile.WP8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using DevDay.Mobile.WP8.Resources;
+
+namespace DevDay.Mobile.WP8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
