8558955488938e0e0db246d6f67d47e77f32eeb1
pawelsawicz <pawel@sawicz.eu>
2014-08-30 13:14:06 +0200

    add speaker and lecture class

---
+++ DevDay.Mobile.WP8.Data/Agenda/Lecture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDay.Mobile.WP8.Data.Agenda
+{
+    public class Lecture
+    {
+        public int IdLecture { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Classroom { get; set; }
+        public string Time { get; set; }
+        public string NameOfPresenter { get; set; }
+        public string Category { get; set; }
+        public string VoteURL { get; set; }
+        public bool IsVoteAble { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ DevDay.Mobile.WP8.Data/Speakers/Speaker.cs
@@ -0,0 +1,20 @@
+﻿using DevDay.Mobile.WP8.Data.Agenda;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDay.Mobile.WP8.Data.Speakers
+{
+    public class Speaker
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string TwitterAccount { get; set; }
+        public string Company { get; set; }
+        public string ImageURL { get; set; }
+        public string ShortName { get; set; }
+        public IList<Lecture> Lectures { get; set; }
+    }
+}
